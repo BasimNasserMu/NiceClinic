b4ca06190d328ea3cf73f315968d1e19e696c5ab
BasimNasserMu <431103991@s.mu.edu.sa>
2026-07-15 19:14:52 +0300

    Changed DB name, Changed Identity Tables names, Initial Migration

--- Models/NiceClinicContext.cs
+++ Models/NiceClinicContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NiceClinic.Models
@@ -7,6 +8,18 @@ namespace NiceClinic.Models
     {
         public NiceClinicContext(DbContextOptions<NiceClinicContext> options) : base(options)
         {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<AppUser>().ToTable("Users", "auth");
+            modelBuilder.Entity<IdentityRole>().ToTable("Roles", "auth");
+            modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "auth");
+            modelBuilder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "auth");
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "auth");
+            modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims", "auth");
+            modelBuilder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "auth");
         }
     }
 }
