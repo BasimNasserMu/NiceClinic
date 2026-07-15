d8a2026e36e80f56f6a12dd463d80dd1c7a39f3a
BasimNasserMu <431103991@s.mu.edu.sa>
2026-07-15 18:52:26 +0300

    Scaffold and AppUser

---
+++ Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NiceClinic.Models
+{
+    public class AppUser: IdentityUser
+    {
+        public string FullName { get; set; } = "New User";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
